e0400f5194af5e24f32d8832429c1be40fbd1446
Andrey Kohanevich <andreikohanevich@gmail.com>
2021-10-10 15:42:56 +0300

    new solutions

---
+++ 1-learnJavaScript.js
@@ -0,0 +1,18 @@
+"use strict"
+
+// =======1========
+
+let admin;
+let name = "John";
+
+admin = name;  // <---  Копируем значение name в admin
+
+// alert(admin);
+
+//=====2===========
+
+const Earth = "Our planet";   // <--- const - неизменяемая переменная. У переменной const обязательно должо быть значение, при его отстутвии будет ошибка: "Uncaught SyntaxError: Missing initializer in const declaration"
+let student = "Visitor of this site";
+
+// =====3==========
+
