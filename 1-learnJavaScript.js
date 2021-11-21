bdd3cd33c1b82c5a4904427994aee2c0adc17c19
Andrey Kohanevich <andreikohanevich@gmail.com>
2021-11-21 10:53:42 +0300

    first alert add

--- 1-learnJavaScript.js
+++ 1-learnJavaScript.js
@@ -1,25 +1,9 @@
-"use strict"
+// first alert
 
-// =======1========
+const myNameIsJavaScript = 'My name is JavaScript';
+alert (myNameIsJavaScript);
 
-let admin;
-let name = "John";
 
-admin = name;  // <---  Копируем значение name в admin
 
-// alert(admin);
 
-//=====2===========
-
-const Earth = "Our planet";   // <--- const - неизменяемая переменная. У переменной const обязательно должо быть значение, при его отстутвии будет ошибка: "Uncaught SyntaxError: Missing initializer in const declaration"
-let student = "Visitor of this site";
-
-// =====3==========
-
-/* let Name = "Ilya";
-alert(`hello ${1}`);
-alert(`hello ${"name"}`);
-alert(`hello ${name}`); */
-
-// =====4===========
 
